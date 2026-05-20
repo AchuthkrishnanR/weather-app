4827c2dc156652baba395920f36de494a423e887
Achuth Krishnan <achuthkrishnanak@gmail.com>
2026-05-20 22:05:21 +0530

    Initial weather app

---
+++ script.js
@@ -0,0 +1,79 @@
+const apiKey = "1ec0a2930f4d54d574c7d798e587a2a6";
+
+const searchBtn =
+    document.getElementById("search-btn");
+
+const cityInput =
+    document.getElementById("city-input");
+
+searchBtn.addEventListener("click", () => {
+
+    const city = cityInput.value;
+
+    getWeather(city);
+
+});
+cityInput.addEventListener("keypress", (event) => {
+
+    if(event.key === "Enter") {
+
+        getWeather(cityInput.value);
+    }
+});
+
+async function getWeather(city) {
+
+    const url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        if(data.cod === "404") {
+
+            alert("City not found");
+
+            return;
+        }
+
+        document.getElementById("city-name")
+            .textContent = data.name;
+
+        document.getElementById("temperature")
+            .textContent =
+                `${Math.round(data.main.temp)}°C`;
+
+        document.getElementById("description")
+            .textContent =
+                data.weather[0].description;
+        const iconCode =
+                data.weather[0].icon;
+
+        const iconUrl =
+        `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+        document.getElementById("weather-icon")
+            .src = iconUrl;
+
+        document.getElementById("humidity")
+            .textContent =
+                `Humidity: ${data.main.humidity}%`;
+
+        document.getElementById("wind")
+            .textContent =
+                `Wind: ${data.wind.speed} km/h`;
+
+    }
+
+    catch(error) {
+
+        console.log(error);
+
+        alert("Something went wrong");
+    }
+}
